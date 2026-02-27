746eea905fc54d3753dc71a498f2f8649b41a4d0
Eyuel Dejene <dejenejoel@gmail.com>
2026-02-27 15:17:09 +0300

    initialize spring

---
+++ src/test/java/com/joel/projectManagement/ProjectManagementApplicationTests.java
@@ -0,0 +1,12 @@
+package com.joel.projectManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+	@Test
+	void contextLoads() {
+		System.out.println("hello world");
+	}
+}
