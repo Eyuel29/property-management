dfe7d77cecee6ff37db14f90f35982edf80e954e
Eyuel Dejene <dejenejoel@gmail.com>
2026-03-16 16:12:07 +0300

    logging and docs

--- src/main/java/com/joel/projectManagement/exception/PMSExceptionHandler.java
+++ src/main/java/com/joel/projectManagement/exception/PMSExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.joel.projectManagement.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -13,15 +15,23 @@ import java.util.List;
 @ControllerAdvice
 public class PMSExceptionHandler {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @ExceptionHandler(MethodArgumentNotValidException.class) public ResponseEntity<List<PMSError>> handleValidation (MethodArgumentNotValidException methodArgumentNotValidException){
         List<FieldError> fieldErrors = methodArgumentNotValidException.getBindingResult().getFieldErrors();
         List<PMSError> pmsErrors = new ArrayList<>();
-        fieldErrors.forEach((fieldError) -> pmsErrors.add(new PMSError(fieldError.getCode(), fieldError.getDefaultMessage())));
+        fieldErrors.forEach((fieldError) -> {
+            logger.error("Inside field validation: {} - {}", fieldError.getField(), fieldError.getDefaultMessage());
+            pmsErrors.add(new PMSError(fieldError.getCode(), fieldError.getDefaultMessage()));
+        });
         return new ResponseEntity(pmsErrors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(PMSException.class)
     public ResponseEntity<List<PMSError>> handle (PMSException pmsException) {
+        pmsException.getErrors().forEach(error -> {
+            logger.error("PMS exception: {} - {}", error.getCode(), error.getMessage());
+        });
         return new ResponseEntity(pmsException.getErrors(), HttpStatus.BAD_REQUEST);
     }
 }
